65fceca48a9f23d56536501d003c7a041387edc1
Avi <Avi@admintheweb.com>
2014-01-05 22:32:03 +0200

    Add Two activities and set new home page

--- src/com/admintheweb/library/HomeActivity.java
+++ src/com/admintheweb/library/HomeActivity.java
@@ -2,12 +2,24 @@ package com.admintheweb.library;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 
 public class HomeActivity extends Activity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+	}
+	
 	public void newNoteBooks (View view){
 		Intent intent = new Intent(this, NewNotebookActivity.class);
+		startActivity(intent);
+	}
+	
+	public void myNoteBooks(View view){
+		Intent intent = new Intent(this, MyNotebooks.class);
 		startActivity(intent);
 	}
 }
